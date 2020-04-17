e6c9585c9a47379c45204383e4b1c7d51e185b46
timonej3 <juho.timonen@aalto.fi>
2020-04-18 02:49:18 +0300

    add shell scripts

---
+++ covid/shell/ab2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -t 0-02:00:00
+#SBATCH --constraint=[ivb|hsw]
+#SBATCH -n 1
+#SBATCH --mem=2500
+#SBATCH -o out/ab.out
+module load r/3.6.1-python3
+srun Rscript --vanilla main_triton.R 10 200 2 0.5
