143c13a32246d00cc24f045c89b5a8c4749545f9
Juho Timonen <juho.timonen@aalto.fi>
2020-04-19 02:39:20 +0300

    new result figure

--- covid/results/plot_result.R
+++ covid/results/plot_result.R
@@ -3,7 +3,15 @@ res <- readRDS('all_results.rds')
 names <- c('euler', 'midpoint', 'adams', 'rk4')
 colors <- c('firebrick', 'steelblue', 'gray20', 'green3')
 
-par(mfrow=c(2,1))
+# Get reference runtimes
+t_rk45 <- mean(res$rk45$TIMES)/60
+t_bdf <- mean(res$bdf$TIMES)/60
+
+
+par(mfrow=c(3,1))
+methods <- c("Euler", "Explicit midpoint", "2-step Adams-Bashforth", "4th order Runge-Kutta")
+
+# Plot A
 plot(c(-2,8), c(0.5,0.5), 'l', xlim=c(0, 6), ylim=c(-0.5, 4), col='orange',
      lty=2, xlab='Step size (days)', ylab='Pareto k')
 grid()
@@ -12,9 +20,25 @@ for(j in 1:4){
   lines(r$h, r$K, col=colors[j])
   points(r$h, r$K, col=colors[j], pch=16)
 }
-legend(3.2, 3.8, lty=1, pch=16, legend=names, col=colors)
+legend(3.2, 4.1, lty=1, pch=16, legend=methods, col=colors, bty='n')
 
+# Plot B
+plot(c(-2,8), c(t_rk45, t_rk45), 'l', xlim=c(0, 6), ylim=c(-0.5, 105), col='red', lwd=1,
+     xlab='Step size (days)', ylab='Average runtime (minutes)')
+lines(c(-2,8), c(t_rk45, t_rk45), 'l', col = 'blue', lty=2, lwd=1)
 
+grid()
+for(j in 1:4){
+  r <- res[[names[j]]]
+  t <- rowMeans(r$TIMES)/60
+  lines(r$h, t, col=colors[j])
+  points(r$h, t, col=colors[j], pch=16)
+}
+legend(1.2, 70.8, lty=1, pch=16, legend=methods, col=colors, bty='n')
+legend(3.0, 94.8, lty=c(1,2), legend=c("RK45 (Stan default tolerances)", "BDF (Stan default tolerances)"), 
+       col=c("red", "blue"), lwd=1, bty='n')
+
+# Plot C
 plot(c(-2,120), c(0.5,0.5), 'l', xlim=c(0, 60), ylim=c(-0.5, 4), col='orange',
      lty=2, xlab='Average runtime (minutes)', ylab='Pareto k')
 grid()
@@ -24,4 +48,4 @@ for(j in 1:4){
   lines(t, r$K, col=colors[j])
   points(t, r$K, col=colors[j], pch=16)
 }
-legend(40, 3.8, lty=1, pch=16, legend=names, col=colors)
+legend(33, 4.0, lty=1, pch=16, legend=methods, col=colors, bty='n')
